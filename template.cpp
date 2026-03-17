7ed78ae6b8b367bfab5807cee466e7d519ea2b79
Cube__ <dbbs.org@gmail.com>
2026-03-17 18:51:15 +0400

    add constants

--- template.cpp
+++ template.cpp
@@ -1,7 +1,20 @@
+// https://github.com/cub33/cpp-template/
+
 #include <bits/stdc++.h>
+#include <numbers>
 using namespace std;
 #define ll long long
 #define db double
+#define ld long double
+#define int128 __int128
+#define endl "\n"
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define sz(x) ((int)(x).size())
+const ld PI numbers::pi;
+const ld EU numbers::e;
+const ll INF 1e18;
+const int MOD 1000000007;
 
 void solve(){
   cout << "Hello world!";
