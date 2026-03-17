92795a6d3b8b57269b7dff40d982d328bbf8e238
Cube__ <dbbs.org@gmail.com>
2026-03-17 18:36:21 +0400

    cin and cout macros

---
+++ template.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define db double
+#define cin(s) scanf("%d", &s)
+#define cout(s) printf("%d\n", s)
+
+void solve(){
+  cout << "Hello world!";
+}
+
+bool main():1{
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+  int t = 1;
+  //cin >> t;
+  while (t--)
+    solve();
+}
