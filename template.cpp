4ec52d14d928d71670f32aa7746d59c6c3ee87dd
Cube__ <dbbs.org@gmail.com>
2026-03-18 10:04:53 +0400

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,35 +1,42 @@
 // https://github.com/cub33/cpp-template/
 
-#pragma GCC optimize ("03")
+#pragma GCC optimize("O3")
+
 #include <bits/stdc++.h>
 #include <numbers>
 using namespace std;
-#define ll long long
-#define db double
-#define ld long double
-#define int128 __int128
-#define endl "\n"
+
+using ll = long long;
+using db = double;
+using ld = long double;
+using int128 = __int128_t;
+
+#define endl '\n'
 #define all(x) (x).begin(), (x).end()
 #define pb push_back
-#define pii pair<int, int>
-#define pll pair<ll, ll>;
 #define sz(x) ((int)(x).size())
-const ld PI numbers::pi;
-const ld EU numbers::e;
-const ll INF 1e18;
-const ll MOD 1000000007;
-mt19937 rng(chrono::stready_clock::now().time_since_epoch().count());
 
-void solve(){
-  cout << "Hello world!";
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+const ld PI = acosl(-1);
+const ld EU = expl(1);
+const ll INF = (ll)1e18;
+const ll MOD = 1000000007;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+void solve() {
+    cout << "Hello world!" << endl;
 }
 
-int main(){
-  ios_base::sync_with_stdio(false);
-  cin.tie(0);
-  cout.tie(0);
-  int t = 1;
-  //cin >> t;
-  while (t--)
-    solve();
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t = 1;
+    // cin >> t;
+
+    while (t--) solve();
 }
