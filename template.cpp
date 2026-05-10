2c869e88035bed7dd31ef2cd731f471a0f8f8cac
Cube__ <dbbs.org@gmail.com>
2026-05-10 13:21:02 +0400

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -7,7 +7,6 @@ using namespace std;
 
 using ll = long long;
 using db = double;
-using ld = long double;
 using int128 = __int128_t;
 
 #define endl '\n'
@@ -19,13 +18,18 @@ using pii = pair<int, int>;
 using pll = pair<ll, ll>;
 #define sc second
 #define fst first
+#define cnl cout << '\n'
 
+const char nl = '\n';
 const ld PI = acosl(-1);
 const ll INF = (ll)1e18;
 const ll MOD = 1000000007;
 
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
+string s = "":
+int n = 0;
+// #define int long long
 void solve() {
     int n;
     cin >> n;
@@ -33,7 +37,7 @@ void solve() {
 }
 
 
-int main() {
+signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
