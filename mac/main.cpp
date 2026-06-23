d7c550d926318e1375e5b14220904a2aede5b7a8
Cube__ <dbbs.org@gmail.com>
2026-06-23 22:22:04 +0300

    Add main.cpp with basic input/output functionality

---
+++ mac/main.cpp
@@ -0,0 +1,28 @@
+// https://github.com/cub33/cpp-template/
+#include "bits/stdc++.h"
+using namespace std;
+using ll = long long;
+
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define sz(x) ((int)(x).size())
+#define int long long
+const char nl = '\n';
+const ll INF = (ll)1e6;
+
+void solve() {
+    int n;
+    cin >> n;
+    cout << n << " is cool"<< nl;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tcs = 1;
+    //cin >> tcs;
+    while (tcs--)
+      solve();
+}
