c19d3f12e52586fd8c1610ae4d60819c7b86d1f1
Cube__ <dbbs.org@gmail.com>
2026-09-23 14:43:15 +0400

    add minimal version

---
+++ extras/min.cpp
@@ -0,0 +1,15 @@
+// https://github.com/cub33/cpp-template/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void solve() {
+    
+}
+int main() {
+    int tcs=1;
+    //cin >> tcs;
+    while (tcs--) {
+        solve();
+    }
+}
