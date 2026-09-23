a6c04684a9ba6735ab34029a2a349e077dc6b9c2
Cube__ <cbrt03606@gmail.com>
2026-09-23 14:47:46 +0400

    Update min.cpp with modern C++ syntax
    Refactor code to use modern C++ features and improve readability.

--- extras/min.cpp
+++ extras/min.cpp
@@ -1,12 +1,18 @@
 // https://github.com/cub33/cpp-template/
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
+using ll = long long;
+#define pb push_back
+const char nl = '\n';
 
 void solve() {
-    
+    int n;
+    cin >> n;
+    cout << "n isnt equal to " << n-1 << nl;
 }
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
     int tcs=1;
     //cin >> tcs;
     while (tcs--) {
